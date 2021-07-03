ebb818d36fed43aba11b3d5891b447ea4dc08cf7
abdulmajid <abdulmajid08180881@gmail.com>
2021-07-03 16:26:41 +0700

    added js

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,3 +1,16 @@
 function insert(num) {
     $('.cal-display').val($('.cal-display').val() + num);
 }
+function multiply(num) {
+    $('.cal-display').val($('.cal-display').val() * num);
+}
+function eql() {
+    $('.cal-display').val(eval($('.cal-display').val()));
+}
+function remove() {
+    $('.cal-display').val('');
+}
+function del() {
+    value = $('.cal-display').val();
+    $('.cal-display').val(value.substring(0, value.length - 1))
+}
